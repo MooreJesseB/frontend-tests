5d9ed010c0945c710f4e1781c26a3e4361a9dfcd
Jesse Moore <moore.jesseb@gmail.com>
2018-12-18 13:49:50 -0800

    Add linear gradients to index element and bottom half of screen

--- slider-ember/app/components/slideshow-screen.js
+++ slider-ember/app/components/slideshow-screen.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
+import { get, computed } from '@ember/object';
 
 export default Component.extend({
+    indexStyle: computed('screen', function() {
+        return 'background: linear-gradient(to top right,' + 
+        get(this, 'screen.colorLeft') + ', ' +
+        get(this, 'screen.colorRight') + ');'
+    }),
+    gradientStyle: computed('screen', function() {
+        return 'background: linear-gradient(to right,' +
+        get(this, 'screen.colorLeft') + ', ' +
+        get(this, 'screen.colorRight') + ');'
+    })
 });
