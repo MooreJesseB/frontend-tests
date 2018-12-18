d787c381ad24316b99f35b8898645bfd98078215
Jesse Moore <moore.jesseb@gmail.com>
2018-12-18 12:45:37 -0800

    Create screen component and stub data to use screen modular

--- slider-ember/app/components/slider-container.js
+++ slider-ember/app/components/slider-container.js
@@ -1,4 +1,31 @@
 import Component from '@ember/component';
+import Array from '@ember';
 
 export default Component.extend({
+    screens: Array([
+        {
+            title: 'Gradients',
+            description: 'Start, end, angle',
+            colorLeft: '#833CF6',
+            colorRight: '#78E6ED',
+            index: '1',
+            icon: ''
+        },
+        {
+            title: 'Presets',
+            description: 'Manage presets',
+            colorLeft: '#78E6ED',
+            colorRight: '#F5F56A',
+            index: '2',
+            icon: ''
+        },
+        {
+            title: 'Colors',
+            description: 'Pick any color',
+            colorLeft: '#F5F56A',
+            colorRight: '#E04487',
+            index: '2',
+            icon: ''
+        },
+    ]),
 });
